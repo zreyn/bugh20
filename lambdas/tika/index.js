4d6f1ce78e1510f63fcc5a1c64eb92ddbd750bab
Zane Reynolds <zane.reynolds@torc.ai>
2021-07-14 11:24:27 -0400

    Adds some implementation

--- lambdas/tika/index.js
+++ lambdas/tika/index.js
@@ -1,4 +1,17 @@
 const bunyan = require('bunyan');
+const AWS = require('aws-sdk');
+const tte = require('tika-text-extract');
+
+const downloadFromS3 = async (key) => {
+  const s3 = new AWS.S3();
+  const s3Params = {
+    Bucket: process.env.S3_BUCKET,
+    Key: key,
+  };
+
+  const { Body } = await s3.getObject(s3Params).promise();
+  return Body;
+};
 
 exports.handler = async (event, context) => {
   const logger = bunyan.createLogger({
@@ -8,6 +21,16 @@ exports.handler = async (event, context) => {
 
   logger.info('handling event', event);
   logger.info('event context', context);
+
+  logger.info(`downloading ${event.pdf_key}`);
+  const pdf = await downloadFromS3(event.pdf_key);
+
+  logger.info('starting tika');
+  await tte.startServer('/tmp/tika-server-1.14.jar');
+
+  logger.info('extracting text');
+  const extractedText = await tte.extract(pdf);
+  logger.info(`${extractedText}`);
 
   return 'success';
 };
--- lambdas/tika/index.test.js
+++ lambdas/tika/index.test.js
@@ -1,5 +1,9 @@
 const bunyan = require('bunyan');
 
+const aws = require('aws-sdk');
+
+aws.config.update({ region: 'us-east-1' });
+
 jest.createMockFromModule('bunyan');
 jest.mock('bunyan');
 const mockBunyanLogger = {
@@ -22,9 +26,28 @@ describe('handler', () => {
     jest.clearAllMocks();
     context = {};
     event = {};
-    process.env.S3_REGION = '';
-    process.env.S3_BUCKET = '';
-    process.env.PDF_PREFIX = '';
-    process.env.EXTRACT_PREFIX = '';
+    process.env.S3_REGION = 'us-east-2';
+    process.env.S3_BUCKET = 'bukkit';
+    process.env.EXTRACT_PREFIX = 'metadata';
+
+    const s3 = jest.fn();
+    aws.S3 = s3;
+    const s3Mock = jest.fn();
+    s3Mock.getObject = jest.fn().mockReturnValue({
+      promise: jest.fn().mockReturnValue(Promise.resolve(true)),
+    });
+    aws.S3.mockImplementation(() => s3Mock);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should log', async () => {
+    event = {
+      pdf_key: 'pages/test.pdf',
+    };
+    await index.handler(event, context);
+    expect(mockBunyanLogger.info).toHaveBeenCalledTimes(6);
   });
 });
